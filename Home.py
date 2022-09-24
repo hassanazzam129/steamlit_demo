d64ed71ca051b61ef73baae85a5fe09ac61775dd
hassanazzam129 <107306505+hassanazzam129@users.noreply.github.com>
2022-09-24 20:30:28 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title ("Hello World")
+
+st.write (" This is test")
+
+st.markdown('''
+               * first item
+               * second item
+               * thrid item
+               * fourth item ''')
+
+st.write (" second test")
+st.markdown ('''
+               * first item
+               * second item
+               * thrid item
+               * fourth item''')
+               
